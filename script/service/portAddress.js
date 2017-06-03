e04ab32ad70dcff0d44d1ffc3f7feeb10e038908
cczhuc <ys1223335@163.com>
2017-06-03 16:31:06 +0800

    接口地址修改

--- script/service/portAddress.js
+++ script/service/portAddress.js
@@ -2,24 +2,24 @@ app.factory("address",function ($state,$http){
     return {
         // 获取banner图,type  0-首页 1-找职位 2-找精英
         banner_url: function (type) {
-            return "/a/article/search?type="+type
+            return "/carrots-ajax/a/article/search?type="+type
         },
 
         //搜索职位, 职位状态 recommend  0-最新职位 1-推荐职位
         searchjob_url: function (type) {
-            return "/a/profession/search?recommend="+type
+            return "/carrots-ajax/a/profession/search?recommend="+type
         },
         //搜索公司, 认证状态  approvedStatus 0-未认证 1-已认证
         searchCompany_url: function (type) {
-            return "/a/company/search?approvedStatus="+type
+            return "/carrots-ajax/a/company/search?approvedStatus="+type
         },
         //职位详情
         jobDetails_url: function (id) {
-            return "/a/profession/"+id
+            return "/carrots-ajax/a/profession/"+id
         },
         //公司详情
         companyDetails_url: function (id) {
-            return "/a/company/"+id
+            return "/carrots-ajax/a/company/"+id
         }
 
     }
